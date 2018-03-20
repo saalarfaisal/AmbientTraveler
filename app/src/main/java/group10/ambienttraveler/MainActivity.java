3ecc7029eb8fd42ecacfc427e6e781317cfd524e
saalarmfaisal@yahoo.ca <saalarmfaisal@yahoo.ca>
2018-03-20 16:29:04 -0400

    I have included the buttons to be used and created the User interface. Saalar

---
+++ app/src/main/java/group10/ambienttraveler/MainActivity.java
@@ -0,0 +1,21 @@
+package group10.ambienttraveler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonOnClick(View v) {
+        Button emergency = (Button) v;
+        ((Button)v).setText("testing");
+    }
+}
