ee01cf126c88387917008e5a0657fc35bdb0ee64
nikhilpai007 <nikhilpaiganesh@trentu.ca>
2018-04-02 14:46:55 -0400

    Activity program to call 911, Minor error at the very end of the code. Someone can fix it ?

--- app/src/main/java/group10/ambienttraveler/MainActivity.java
+++ app/src/main/java/group10/ambienttraveler/MainActivity.java
@@ -1,33 +1,43 @@
 package group10.ambienttraveler;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
+public abstract class MainActivity extends AppCompatActivity {
 
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b= (Button) findViewById(R.id.call);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent callIntent=new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:911"));
+                startActivity(callIntent);
+
+
+            }
+        });
+
+
     }
 
-    public void Police (View view){
-        String number = "911";
-        Intent intent = new Intent(Intent.ACTION_CALL);
-        intent.setData(Uri.parse("tel:" + number));
-        if (ActivityCompat.checkSelfPermission (this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
-
-        return;
-    }
 
 
-    }
+
+
+
 }
+
